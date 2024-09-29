16ac2b4b7c5b3c2ea6611345031a943b0181a0d3
Jean Luc Houédanou <jhouedanou@gmail.com>
2024-09-29 21:49:33 +0000

    no aos

--- app.vue
+++ app.vue
@@ -3,12 +3,12 @@
     <section class="hero is-fullheight">
       <div class="hero-body">
         <div class="container has-text-centered">
-          <h1 class="title is-1" data-aos="fade-down">Site en construction</h1>
-          <h2 class="subtitle is-3" data-aos="fade-up" data-aos-delay="200">
+          <h1 class="title is-1">Site en construction</h1>
+          <h2 class="subtitle is-3">
             Nous travaillons dur pour vous offrir une expérience exceptionnelle
             !
           </h2>
-          <div class="mt-6" data-aos="zoom-in" data-aos-delay="400">
+          <div class="mt-6">
             <span class="icon is-large">
               <i class="fas fa-cog fa-spin fa-3x"></i>
             </span>
@@ -20,18 +20,8 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import "bulma/css/bulma.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
-onMounted(() => {
-  AOS.init({
-    duration: 1000,
-    once: true,
-  });
-});
 </script>
 
 <style>
