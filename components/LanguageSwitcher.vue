3a3ebb52b6c2f4a0309d1a96245fb21586d3480a
Jean Luc Houédanou <jhouedanou@gmail.com>
2024-10-19 01:06:00 +0000

    bug lng

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,22 +1,27 @@
 <template>
     <div class="language-switcher">
-      <select v-model="$i18n.locale" @change="changeLanguage">
-        <option v-for="locale in $i18n.locales" :key="locale.code" :value="locale.code">
-          {{ locale.name }}
-        </option>
-      </select>
+        <select v-model="$i18n.locale" @change="changeLanguage">
+            <option v-for="locale in $i18n.locales" :key="locale.code" :value="locale.code">
+                {{ locale.name }}
+            </option>
+        </select>
     </div>
 </template>
 
 <script setup>
+import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
+import { useSwitchLocalePath } from '#i18n'
+
 const { locale, locales } = useI18n()
 const router = useRouter()
+const switchLocalePath = useSwitchLocalePath()
 
 function changeLanguage(event) {
     const newLocale = event.target.value
     router.push(switchLocalePath(newLocale))
     setTimeout(() => {
-      window.location.reload()
+        window.location.reload()
     }, 100)
 }
 </script>
