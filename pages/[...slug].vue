ade611f4118035df19c0d0747c5cc7ed08379987
Jean Luc Houédanou <jhouedanou@gmail.com>
2025-03-12 18:42:33 +0000

    update og

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -68,12 +68,11 @@ useHead({
       content: article.value.description || "Description par défaut",
     },
     { property: "og:url", content: `https://houedanou.com${route.path}` },
-    { property: "og:image", content: article.value.image },
+    { property: "og:image", content: `https://houedanou.com${article.value.image}` },
     { property: "og:site_name", content: "Jean-Luc Houédanou" },
     { name: "twitter:card", content: "summary_large_image" },
-    { name: "twitter:image", content: article.value.image },
-    { name: "twitter:title", content: article.value.title },
-    {
+    { name: "twitter:image", content: `https://houedanou.com${article.value.image}` },
+    { name: "twitter:title", content: article.value.title },    {
       name: "twitter:description",
       content: article.value.description || "Description par défaut",
     },
